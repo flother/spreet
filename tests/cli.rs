cd5533223432eae83fc7bb6f7e570d5b11188469
Matt Riggott <matt.riggott@gmail.com>
2022-03-06 20:54:21 +0000

    Fix failing test of the spreet CLI

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 fn spreet_can_run_successfully() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("spreet")?;
 
-    cmd.arg("input").arg("output").arg("--ratio").arg("2");
+    cmd.arg(".").arg("output").arg("--ratio").arg("2");
     cmd.assert().success();
 
     Ok(())
