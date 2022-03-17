ed143ba41e56688b54370faa993f1f59690f504c
Matt Riggott <matt.riggott@gmail.com>
2022-03-17 15:53:36 +0000

    Use char, not str, for single-character constant

--- src/fs.rs
+++ src/fs.rs
@@ -14,7 +14,7 @@ fn is_hidden(entry: &DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
-        .map(|s| s.starts_with("."))
+        .map(|s| s.starts_with('.'))
         .unwrap_or(false)
 }
 
