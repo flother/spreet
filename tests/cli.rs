898e77fa55592bdbbf186890a6192d4c32b9d2f3
Matt Riggott <matt.riggott@gmail.com>
2022-03-18 10:18:04 +0000

    Add SVG to use as test input

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,7 +5,10 @@ use std::process::Command;
 fn spreet_can_run_successfully() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("spreet")?;
 
-    cmd.arg(".").arg("output").arg("--ratio").arg("2");
+    cmd.arg("tests/svgs")
+        .arg("test_output@2x")
+        .arg("--ratio")
+        .arg("2");
     cmd.assert().success();
 
     Ok(())
