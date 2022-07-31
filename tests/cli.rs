80c8fb4b89b40eab4cd368ee583f5eaa6c78e956
Matt Riggott <matt.riggott@gmail.com>
2022-07-31 12:51:19 +0000

    Test for correct program output
    The previous test only checked that the spreet CLI could run
    successfully. Now it actually checks that the output is correct: that
    the `--ratio` changes the size of the sprites, and that the `--unique`
    flag removes duplicate sprites from the spritesheet and correctly
    references them by multiple names in the index. The test run output is
    now put into a temp directory rather than the root of the repo.

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,15 +1,108 @@
-use assert_cmd::prelude::*;
+use std::path::Path;
 use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
 
 #[test]
 fn spreet_can_run_successfully() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("spreet")?;
+    let temp = assert_fs::TempDir::new().unwrap();
 
-    cmd.arg("tests/svgs")
-        .arg("test_output@2x")
+    let mut cmd = Command::cargo_bin("spreet")?;
+    cmd.arg("tests/fixtures/svgs")
+        .arg(temp.join("can_run"))
         .arg("--ratio")
-        .arg("2");
-    cmd.assert().success();
+        .arg("2")
+        .assert()
+        .success();
+
+    Ok(())
+}
+
+#[test]
+fn spreet_can_output_spritesheet() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("spreet")?;
+    cmd.arg("tests/fixtures/svgs")
+        .arg(temp.join("default"))
+        .assert()
+        .success();
+
+    let expected_spritesheet = Path::new("tests/fixtures/output/default@1x.png");
+    let actual_spritesheet = predicate::path::eq_file(temp.join("default.png"));
+    let expected_index = Path::new("tests/fixtures/output/default@1x.json");
+    let actual_index = predicate::path::eq_file(temp.join("default.json"));
+
+    assert!(actual_spritesheet.eval(expected_spritesheet));
+    assert!(actual_index.eval(expected_index));
+
+    Ok(())
+}
+
+#[test]
+fn spreet_can_output_unique_spritesheet() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("spreet")?;
+    cmd.arg("tests/fixtures/svgs")
+        .arg(temp.join("unique"))
+        .arg("--unique")
+        .assert()
+        .success();
+
+    let expected_spritesheet = Path::new("tests/fixtures/output/unique@1x.png");
+    let actual_spritesheet = predicate::path::eq_file(temp.join("unique.png"));
+    let expected_index = Path::new("tests/fixtures/output/unique@1x.json");
+    let actual_index = predicate::path::eq_file(temp.join("unique.json"));
+
+    assert!(actual_spritesheet.eval(expected_spritesheet));
+    assert!(actual_index.eval(expected_index));
+
+    Ok(())
+}
+
+#[test]
+fn spreet_can_output_retina_spritesheet() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("spreet")?;
+    cmd.arg("tests/fixtures/svgs")
+        .arg(temp.join("default@2x"))
+        .arg("--retina")
+        .assert()
+        .success();
+
+    let expected_spritesheet = Path::new("tests/fixtures/output/default@2x.png");
+    let actual_spritesheet = predicate::path::eq_file(temp.join("default@2x.png"));
+    let expected_index = Path::new("tests/fixtures/output/default@2x.json");
+    let actual_index = predicate::path::eq_file(temp.join("default@2x.json"));
+
+    assert!(actual_spritesheet.eval(expected_spritesheet));
+    assert!(actual_index.eval(expected_index));
+
+    Ok(())
+}
+
+#[test]
+fn spreet_can_output_unique_retina_spritesheet() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("spreet")?;
+    cmd.arg("tests/fixtures/svgs")
+        .arg(temp.join("unique@2x"))
+        .arg("--retina")
+        .arg("--unique")
+        .assert()
+        .success();
+
+    let expected_spritesheet = Path::new("tests/fixtures/output/unique@2x.png");
+    let actual_spritesheet = predicate::path::eq_file(temp.join("unique@2x.png"));
+    let expected_index = Path::new("tests/fixtures/output/unique@2x.json");
+    let actual_index = predicate::path::eq_file(temp.join("unique@2x.json"));
+
+    assert!(actual_spritesheet.eval(expected_spritesheet));
+    assert!(actual_index.eval(expected_index));
 
     Ok(())
 }
