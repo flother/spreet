c9cdc1fc1f7ef7eb30dfc45e2e31c1c6642b6457
Matt Riggott <matt.riggott@gmail.com>
2022-10-26 19:28:40 +0000

    Remove deprecated Clap features

--- src/bin/spreet/cli.rs
+++ src/bin/spreet/cli.rs
@@ -9,12 +9,12 @@ use clap::{ArgGroup, Parser};
 #[clap(group(ArgGroup::new("pixel_ratio").args(&["ratio", "retina"])))]
 pub struct Cli {
     /// A directory of SVGs to include in the spritesheet
-    #[clap(validator = is_dir)]
+    #[clap(value_parser = is_dir)]
     pub input: PathBuf,
     /// Name of the file in which to save the spritesheet
     pub output: String,
     /// Set the output pixel ratio
-    #[clap(short, long, default_value_t = 1, validator = is_positive)]
+    #[clap(short, long, default_value_t = 1, value_parser = is_positive)]
     pub ratio: u8,
     /// Set the pixel ratio to 2 (equivalent to `--ratio=2`)
     #[clap(long)]
@@ -28,19 +28,19 @@ pub struct Cli {
 }
 
 /// Clap validator to ensure that a string is an existing directory.
-fn is_dir(p: &str) -> Result<(), String> {
+fn is_dir(p: &str) -> Result<PathBuf, String> {
     match PathBuf::from(p).is_dir() {
-        true => Ok(()),
+        true => Ok(p.into()),
         false => Err(String::from("must be an existing directory")),
     }
 }
 
 /// Clap validator to ensure that an unsigned integer parsed from a string is greater than zero.
-fn is_positive(s: &str) -> Result<(), String> {
+fn is_positive(s: &str) -> Result<u8, String> {
     u8::from_str(s)
         .map_err(|e| e.to_string())
         .and_then(|result| match result {
-            i if i > 0 => Ok(()),
+            i if i > 0 => Ok(result),
             _ => Err(String::from("must be greater than one")),
         })
 }
