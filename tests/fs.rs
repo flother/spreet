f6e893a4bd3439ed57b6b949c8aca4d7f481b48a
Matt Riggott <matt.riggott@gmail.com>
2023-10-12 14:23:58 +0000

    Stabilise test

--- tests/fs.rs
+++ tests/fs.rs
@@ -19,13 +19,15 @@ fn get_svg_input_paths_returns_non_recursive_results() {
 
 #[test]
 fn get_svg_input_paths_returns_recursive_results() {
+    let mut input_paths = get_svg_input_paths(Path::new("tests/fixtures/svgs"), true).unwrap();
+    input_paths.sort();
     assert_eq!(
-        get_svg_input_paths(Path::new("tests/fixtures/svgs"), true).unwrap(),
+        input_paths,
         vec![
-            Path::new("tests/fixtures/svgs/circle.svg"),
             Path::new("tests/fixtures/svgs/another_bicycle.svg"),
-            Path::new("tests/fixtures/svgs/recursive/bear.svg"),
             Path::new("tests/fixtures/svgs/bicycle.svg"),
+            Path::new("tests/fixtures/svgs/circle.svg"),
+            Path::new("tests/fixtures/svgs/recursive/bear.svg"),
         ]
     );
 }
